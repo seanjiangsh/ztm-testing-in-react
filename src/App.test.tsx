c91d7f394845423b3b3634f3a24b19a74e089ddf
sean <seanjiangsh@gmail.com>
2023-05-11 15:52:08 +0800

    Added App title text test with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+import { shallow } from "enzyme";
+
 import App from "./App";
 
 test("renders 'Monsters Rolodex' in App", () => {
   render(<App />);
   const linkElement = screen.getByText(/Monsters Rolodex/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("expect 'Monsters Rolodex' in App title", () => {
+  expect.assertions(2);
+  const app = shallow(<App />);
+  const title = app.find(".app-title");
+  expect(title).toHaveLength(1);
+  expect(title.text()).toEqual("Monsters Rolodex");
 });
